d2498be6ce2e7e24f6eb46a88d0e17f9a97d6505
ohagi0729 <mktrx782@gmail.com>
2024-02-13 13:32:54 +0000

    Initial Commit

---
+++ 14_/index.rb
@@ -0,0 +1,84 @@
+# 「こんにちは、Ruby」と出力してください
+puts "こんにちは、Ruby"
+
+# 以下の行をコメントにしてください
+# puts "Hello Ruby"
+
+# 数値の37を出力してください
+puts 37
+
+# 「+」を用いて、2に9を足した値を出力してください
+puts 2 + 9
+
+# 文字列「2 + 9」を出力してください
+puts "2 + 9"
+
+# 「*」を用いて、13に9を掛けた値を出力してください
+puts 13 * 9
+
+# 「/」を用いて、32を8で割った値を出力してください
+puts 32 / 8
+
+# 「%」を用いて、18を5で割った余りを出力してください
+puts 18 % 5
+
+# 「私は」と「にんじゃわんこです」を連結して出力してください
+puts "私は" + "にんじゃわんこです"
+
+# 文字列の「38」と「19」を連結して出力してください
+puts "38" + "19"
+
+# 変数nameに、文字列「にんじゃわんこ」を代入してください
+name = "にんじゃわんこ"
+
+# 変数nameを出力してください
+puts name
+
+# 文字列「Progateで」と変数textを連結して、出力してください
+text = "プログラミングを学ぼう"
+puts "Progateで" + text
+
+# 変数lengthと変数widthを掛けて出力してください
+length = 8
+width = 9
+puts length * width
+
+# 変数textに「をマスターしよう」を代入してください
+text = "をマスターしよう"
+
+# 下記の文字列に変数textを連結してください
+puts "HTML" + text
+puts "CSS" + text
+puts "Ruby" + text
+
+# 変数widthの値を13に変更してください
+length = 9
+width = 8
+puts width
+puts length * width
+puts "----"
+
+width = 13
+
+puts width
+puts length * width
+
+# widthの値に5を足して、widthの値を変更してください
+length = 9
+width = 8
+puts width
+puts length * width
+
+puts "----"
+
+width += 5
+
+puts width
+puts length * width
+
+# 「面積は◯◯です」となるように出力してください
+length = 9
+width = 8
+area = length * width
+
+puts "面積は#{area}です"
